0aad61bc513539a70dc987ec5d4e3ffe434a9ee2
Carlos Michael <carlosmichaeldev@gmail.com>
2024-03-21 22:25:02 -0400

    Update game flow manager and world scene

--- FRESH-FLOWERS/Scripts/fit_to_window.gd
+++ FRESH-FLOWERS/Scripts/fit_to_window.gd
@@ -6,16 +6,16 @@ extends Node
 @export var reset_size : bool = true
 
 func _ready():
-    # Responsive code to Window Size changes
-    get_tree().get_root().size_changed.connect(_on_size_changed)
-    call_deferred("_on_size_changed")
+	# Responsive code to Window Size changes
+	get_tree().get_root().size_changed.connect(_on_size_changed)
+	call_deferred("_on_size_changed")
 
 func _on_size_changed():
-    # Reset -- Not sure why this is neccessary but it seems to be.
-    if reset_size:
-        set_node.size = Vector2(0,0)
-    # Auto-sizing
-    if fit_width:
-        set_node.size.x = get_viewport().size.x
-    if fit_height:
-        set_node.size.y = get_viewport().size.y
+	# Reset -- Not sure why this is neccessary but it seems to be.
+	if reset_size:
+		set_node.size = Vector2(0,0)
+	# Auto-sizing
+	if fit_width:
+		set_node.size.x = get_viewport().size.x
+	if fit_height:
+		set_node.size.y = get_viewport().size.y
