0bbf475eebe71d6cee6241c0cb28eeb721066800
Carlos Michael <carlosmichaeldev@gmail.com>
2024-04-03 23:24:12 -0400

    Delete loading_screen.gd and main-menu-3d.gd

--- FRESH-FLOWERS/Scripts/main-menu-3d.gd
+++ FRESH-FLOWERS/Scripts/main-menu-3d.gd
@@ -4,6 +4,7 @@ class_name MainMenu3D
 @onready var color_rect : ColorRect = %ColorRect
 @export var button : Button
 @export var main_menu_music : AudioStreamPlayer
+@export var main_menu_stinger : AudioStreamPlayer
 @export var camera : Camera3D
 @export var version_label : Label
 
@@ -16,13 +17,13 @@ func _ready() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(camera, "position:z", 4.7, 1.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(camera, "position:z", 4.6, 2.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	# tween.set_loops(-1)
+	# tween.set_loops(5)
 
 	var new_tween = get_tree().create_tween()
 	new_tween.tween_property(camera, "rotation_degrees:x", -1.5, .4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	new_tween.tween_property(camera, "rotation_degrees:x", -.9, .4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	new_tween.tween_property(camera, "rotation_degrees:x", 0, 2.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	# new_tween.set_loops(-1)
+	# new_tween.set_loops(5)
 	# get version number from project settings
 	version_label.text = "unwilting-" + ProjectSettings.get_setting("application/config/version")
 
@@ -39,6 +40,7 @@ func fade_out(duration):
 func exit_fade():
 	button.hide()
 	main_menu_music.stop()
+	main_menu_stinger.play()
 	fade_out(2)
 
 
